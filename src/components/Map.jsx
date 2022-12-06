89d991b07bbe717ee2d2447e812e9f29bade95d4
francoosman12 <francoosman12@gmail.com>
2022-12-05 21:10:57 -0300

    actualice git ignore

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import GoogleMaps from "simple-react-google-maps";
+import "../js/MapKey";
+import MapKey from "../js/MapKey";
 
 const Map = () => {
   return (
     <div className="container">
       <GoogleMaps
-        apiKey={"AIzaSyDUzYklxBrA5JHYlcsX0KACSnlqzS34d2s"}
+        apiKey={`${MapKey.apiKey}`}
         style={{ height: "600px", width: "100%" }}
         zoom={15}
         center={{ lat: -26.831712915403326, lng: -65.20454648224023 }}
